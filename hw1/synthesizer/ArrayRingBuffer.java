84ba5f291aff35f37b1c6b1570d8b0fecd82a20e
hui <1018997102@qq.com>
2020-12-10 16:35:26 +0800

    completed hw1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -36,13 +36,12 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
 
     @Override
     public void enqueue(Object x) {
-        try{
-            fillCount++;
-            rb[last] = (T)x;
-            last = (last + 1) % capacity;
-        }catch (Exception e){
-            e.printStackTrace();
+        if (fillCount == capacity) {
+            throw new RuntimeException("you can't add more elements");
         }
+        fillCount++;
+        rb[last] = (T)x;
+        last = (last + 1) % capacity;
     }
 
     /**
@@ -52,15 +51,13 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      */
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
-        try{
-            fillCount--;
-            T temp = rb[first];
-            first = (first + 1) % capacity;
-            return temp;
-        }catch (Exception e){
-            e.printStackTrace();
+        if(fillCount == 0) {
+            throw new RuntimeException("you can't delete element");
         }
-       return null;
+        fillCount--;
+        T temp = rb[first];
+        first = (first + 1) % capacity;
+        return temp;
     }
 
     /**
@@ -68,13 +65,11 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
-       try{
-           T temp = rb[(first % capacity)];
-           return temp;
-       }catch (Exception e){
-           e.printStackTrace();
-       }
-       return null;
+        if(fillCount == 0) {
+            throw new RuntimeException("you can't peek element");
+        }
+        T temp = rb[(first % capacity)];
+        return temp;
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,4 +1,4 @@
-// TODO: Make sure to make this class a part of the synthesizer package
+// todo: Make sure to make this class a part of the synthesizer package
 package synthesizer;
 
 //Make sure this class is public
@@ -14,7 +14,7 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
+        // todo: Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this divsion operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
@@ -26,7 +26,7 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
+        // todo: Dequeue everything in the buffer, and replace it with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
         //
@@ -42,7 +42,7 @@ public class GuitarString {
      * the Karplus-Strong algorithm. 
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
+        // todo: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
 
